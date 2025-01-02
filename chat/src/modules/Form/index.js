785f814fe0360957bc03caaf4c6dbde9058650e2
Anjali Methre <sonaanjali2211@gmail.com>
2025-01-02 20:06:16 +0530

    adding Form

---
+++ chat/src/modules/Form/index.js
@@ -0,0 +1,15 @@
+import Input from "../../components/Input"
+
+const Form = () => {
+  return (
+    <div className="bg-white w-[400px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+        <div className="text-4xl font-extrabold">Welcome</div>
+        <div className="text-xl font-light mb-14" >Sign up now to get started </div>
+        <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6"/>
+        <Input label="Email address" name="email" placeholder="Enter your email" className="mb-6"/>
+        <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-6"/>
+    </div>
+  )
+}
+
+export default Form
