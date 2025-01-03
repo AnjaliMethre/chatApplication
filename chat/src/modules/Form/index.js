f798f3a7da7b845ebcacdef9f576c97d3a62593a
Anjali Methre <sonaanjali2211@gmail.com>
2025-01-03 22:12:57 +0530

    added sign up button

--- chat/src/modules/Form/index.js
+++ chat/src/modules/Form/index.js
@@ -1,4 +1,6 @@
+import Button from "../../components/Button"
 import Input from "../../components/Input"
+
 
 const Form = () => {
   return (
@@ -7,7 +9,9 @@ const Form = () => {
         <div className="text-xl font-light mb-14" >Sign up now to get started </div>
         <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6"/>
         <Input label="Email address" name="email" placeholder="Enter your email" className="mb-6"/>
-        <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-6"/>
+        <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-14"/>
+        <Button label="Sign up" className="w-1/2 mb-2"/>
+        <div>Already have an account? <span className="text-primary cursor-pointer underline">Sign in</span></div>
     </div>
   )
 }
