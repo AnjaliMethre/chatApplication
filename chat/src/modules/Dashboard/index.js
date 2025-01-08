cd848a4c6389043a93c1880486d3c6061db4bcae
Anjali Methre <sonaanjali2211@gmail.com>
2025-01-08 20:59:11 +0530

    frontend part completed

--- chat/src/modules/Dashboard/index.js
+++ chat/src/modules/Dashboard/index.js
@@ -1,4 +1,5 @@
 import Avatar from '../../assets/avatar.png'
+import Input from '../../components/Input/index.js'
 
 const Dashboard = () => {
   const contacts=[
@@ -70,22 +71,68 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      <div className='w-[50%]  h-screen bg-white flex flex-col items-center'>
+      <div className='w-[75%]  h-screen bg-white flex flex-col items-center'>
         <div className='w-[75%] bg-secondary h-[80px] mt-14 rounded-full flex items-center px-14'>
-          <div><img src={Avatar} width={60} height={60} /></div>
+          <div className='cursor-pointer'><img src={Avatar} width={60} height={60} /></div>
           <div className='ml-6 mr-auto'>
             <h3 className='text-lg'>Alexander</h3>
             <p className='text-sm font-light text-gray-600'>online</p>
           </div>
-          <div>
+          <div className='cursor-pointer'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2"> 
           <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2c-8.072 -.49 -14.51 -6.928 -15 -15a2 2 0 0 1 2 -2"></path> 
           <path d="M15 5h6"></path> <path d="M18.5 7.5l2.5 -2.5l-2.5 -2.5"></path> </svg> 
           </div>
           
         </div>
+          <div className='h-[50%] w-full overflow-scroll shadow-sm'>
+            <div className='p-14'>
+              <div className='max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              <div className='max-w-[40%] bg-primary rounded-b-xl rounded-tr-xl ml-auto p-4 text-white mb-6'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </div>
+              
+            </div>
+          </div>
+          <div className='p-14 w-full flex items-center'>
+            <Input placeholder='Type a message...' className='w-[75%]' inputClassName='p-4 border-0 shadow-md 
+            rounded-full bg-light focus:ring-0 focus:border-0 outline-none'/>
+            <div className='ml-4 p-4 cursor-pointer bg-light rounded-full'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="1.5">
+              <path d="M10 14l11 -11"></path> 
+              <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path> </svg> 
+            </div>
+            <div className='ml-4 p-4 cursor-pointer bg-light rounded-full'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path> <path d="M9 12h6"></path> 
+              <path d="M12 9v6"></path> </svg> 
+            </div>
+          </div>
       </div>
-      <div className='w-[25%]  h-screen'></div>
+      
+      <div className='w-[25%]  h-screen bg-light'>
+
+      </div>
     </div>
   )
 }
--- chat/src/modules/Form/index.js
+++ chat/src/modules/Form/index.js
@@ -1,7 +1,7 @@
 import { useState} from "react"
 import Button from "../../components/Button"
 import Input from "../../components/Input"
-
+import {Navigate, useNavigate} from 'react-router-dom'
 
 const Form = ({
   isSignInPage=true,  
@@ -16,20 +16,22 @@ const Form = ({
 
 
   
-  
+  const navigate=useNavigate()
   return (
-    <div className="bg-white w-[400px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
-        <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
-        <div className="text-xl font-light mb-14" >{isSignInPage ? 'Sign up to get explore':'Sign up to get started'} </div>
-        <form className="flex flex-col items-center w-full" onSubmit={()=>console.log('Submitted')} >
-        {!isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6" value={data.fullName}  onChange={(e)=>setdata({...data,fullName:e.target.value})} />}
+    <div className="bg-light h-screen flex items-center justify-center">
+      <div className="bg-white w-[400px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+          <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
+          <div className="text-xl font-light mb-14" >{isSignInPage ? 'Sign up to get explore':'Sign up to get started'} </div>
+          <form className="flex flex-col items-center w-full" onSubmit={()=>console.log('Submitted')} >
+          {!isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6 w-[50%]" value={data.fullName}  onChange={(e)=>setdata({...data,fullName:e.target.value})} />}
+          
+          <Input label="Email address" type="email" name="email" placeholder="Enter your email" className="mb-6 w-[50%]" value={data.email} onChange={(e)=>setdata({...data,email:e.target.value})}/>
+          <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-14 w-[50%]" value={data.password} onChange={(e)=>setdata({...data,password:e.target.value})} />
+          <Button label={isSignInPage ? 'Sign in':'Sign up'} type='submit' className="w-1/2 mb-2"/>
+          </form>
         
-        <Input label="Email address" type="email" name="email" placeholder="Enter your email" className="mb-6" value={data.email} onChange={(e)=>setdata({...data,email:e.target.value})}/>
-        <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-14" value={data.password} onChange={(e)=>setdata({...data,password:e.target.value})} />
-        <Button label={isSignInPage ? 'Sign in':'Sign up'} type='submit' className="w-1/2 mb-2"/>
-        </form>
-       
-        <div>{isSignInPage ? "Didn't have an account" : "Already have an account?"} <span className="text-primary cursor-pointer underline">{isSignInPage ? 'Sign up' : 'Sign in'}</span></div>
+          <div>{isSignInPage ? "Didn't have an account" : "Already have an account?"} <span className="text-primary cursor-pointer underline" onClick={()=>navigate(`/users/${isSignInPage?'sign_up':'sign_in'}`)}>{isSignInPage ? 'Sign up' : 'Sign in'}</span></div>
+      </div>
     </div>
   )
 }
