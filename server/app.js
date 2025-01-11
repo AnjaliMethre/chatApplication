e0af92e5378204892be762325a1064e280ba4b5c
Anjali Methre <sonaanjali2211@gmail.com>
2025-01-11 23:19:27 +0530

    added messages

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ require('./db/connection');
 //Import files
 const Users=require('./models/Users');
 const Conversations=require('./models/Conversations');
+const Messages = require('./models/Messages');
 
 //app Use
 const app=express();
@@ -122,6 +123,57 @@ app.get('/api/conversation/:userId', async (req, res)=>{
     }
 })
 
+app.post('/api/message',async(req,res)=>{
+    try{
+        const {conversationId, senderId, message, receiverId=''}=req.body;
+        if(!senderId || !message) return res.status(400).send('please fill all required fields')
+        if(!conversationId && receiverId){
+            const newConversation=new conversations({members:[senderId, receiverId]});
+            await newConversation.save();
+            const newMessage=new Messages({conversationId: newConversation._id, senderId, message});
+            await newMessage.save();
+            return res=status(200).send('Message sent successfully');
+        }
+        else if(!conversationId && !receiverId){
+            return res.status(400).send('Please fill all required fields')
+        }
+        const newMessage=new Messages({conversationId, senderId, message });
+        await newMessage.save();
+        res.status(200).send('Message sent successfully');
+    }
+    catch(error){
+        console.log(error, 'Error')
+    }
+})
+
+app.get('/api/message/:conversationId',async(req,res)=>{
+    try{
+        const conversationId=req.params.conversationId;
+        if(!conversationId=='new') return res.status(200).json([]);
+        const messages=await Messages.find({conversationId});
+        const messageUserData=Promise.all(messages.map(async(message)=>{
+         const user=await Users.findById(message.senderId);
+         return {user: {email:user.email, fullName:user.fullName}, message:message.message}
+        }));
+     res.status(200).json(await messageUserData);
+    }
+    catch(error){
+        console.log('Error',error)
+    }
+})
+
+app.get('/api/users',async(req, res)=>{
+    try{
+        const users=await Users.find();
+        const UsersData=Promise.all(users.map(async (user)=>{
+            return {user:{email:user.email, fullName:user.fullName }, userId:user._id }
+        }))
+        res.status(200).json(await usersData);
+    }
+    catch(error){
+        console.log('Error',error)
+    }
+})
 app.listen(port, ()=>{
     console.log('listening on port ' +port);
 })
