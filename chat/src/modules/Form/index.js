8ec79063cb2dab425827ad03e84e2d484bf80639
Anjali Methre <sonaanjali2211@gmail.com>
2025-01-12 21:38:42 +0530

    adding data

--- chat/src/modules/Form/index.js
+++ chat/src/modules/Form/index.js
@@ -14,15 +14,41 @@ const Form = ({
     password:''
   })
 
-
-  
   const navigate=useNavigate()
+
+  const handleSubmit=async(e)=>{
+    console.log('data:>>',data);
+    e.preventDefault()
+    const res=await fetch(`http://localhost:8000/api/${isSignInPage ? 'login' : 'register'}`, {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    })
+
+    if(res.status===400){
+      alert('Invalid credentials')
+    }
+    else{
+      const resData=await res.json()
+      console.log('data:>>',resData);
+      if(resData.token){
+        localStorage.setItem('user:token',resData.token)
+        localStorage.setItem('user:detail',resData.user)
+        navigate('/')
+      }
+      
+    }
+   
+
+  }
   return (
     <div className="bg-light h-screen flex items-center justify-center">
       <div className="bg-white w-[400px] h-[600px] shadow-lg rounded-lg flex flex-col justify-center items-center">
           <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
           <div className="text-xl font-light mb-14" >{isSignInPage ? 'Sign up to get explore':'Sign up to get started'} </div>
-          <form className="flex flex-col items-center w-full" onSubmit={()=>console.log('Submitted')} >
+          <form className="flex flex-col items-center w-full" onSubmit={(e)=> handleSubmit(e)} >
           {!isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6 w-[50%]" value={data.fullName}  onChange={(e)=>setdata({...data,fullName:e.target.value})} />}
           
           <Input label="Email address" type="email" name="email" placeholder="Enter your email" className="mb-6 w-[50%]" value={data.email} onChange={(e)=>setdata({...data,email:e.target.value})}/>
