7f1e66822bbea8d3ef2898874448f62de4bfcb10
Anjali Methre <113003808+AnjaliMethre@users.noreply.github.com>
2025-01-28 15:47:54 +0530

    Delete server directory

--- server/db/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const url=`mongodb+srv://chat_app_admin:1234Anjali@cluster0.8vfwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.connect(url).then(()=>console.log('Connected to DB')).catch((e)=>console.log('Error',e))
